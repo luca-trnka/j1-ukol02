ae1162b04e45b205723bec30527169de291c7b5c
luca-trnka <luie-trnkova76@seznam.cz>
2022-04-24 16:00:47 +0200

    2.část zmrzka a snehulak

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -9,67 +9,99 @@ public class HlavniProgram {
     public void main(String[] args) {
         Turtle anakin;
         anakin = new Turtle();
-        anakin.setLocation(100, 150);
-        nakresliTrojuhelnik(anakin, 100.0, Color.blue);
 
-        Turtle padme;
-        padme = new Turtle();
-        padme.setLocation(250, 150);
-        nakresliCtverec(padme, 100.0, Color.red);
 
-        Turtle leia;
-        leia = new Turtle();
-        leia.setLocation(400, 150);
-        nakresliObdelnik(leia, 100.0, 150.0, Color.yellow);
+      //  nakresliZmrzku(anakin);
 
-        Turtle luke;
-        luke = new Turtle();
-        luke.setLocation(600, 100);
-        nakresliKolo(luke, 50.0, Color.green);
+        nakresliSnehulaka(anakin);
 
 
     }
 
-    private void nakresliTrojuhelnik(Turtle anakin, double velikostStrany, Color barva) {
+    private void nakresliSnehulaka(Turtle anakin) {
+        anakin.setLocation(500, 90);
+        nakresliKolo(anakin, 50, Color.cyan);
+        anakin.setLocation(539.34, 133.81);
+        anakin.turnRight(90);
+        nakresliKolo(anakin, 75, Color.cyan);
+        anakin.setLocation(539.14, 283.26);
+        nakresliKolo(anakin, 100, Color.cyan);
+        anakin.setLocation(471.95, 208.64);
+        anakin.turnRight(90);
+        nakresliKolo(anakin, 30, Color.cyan);
+        anakin.setLocation(622.23, 212.24);
+        anakin.turnLeft(180);
+        nakresliKolo(anakin, 30, Color.cyan);
+    }
+
+    private void nakresliZmrzku(Turtle anakin) {
+        anakin.setLocation(100, 200);
+        nakresliKolo (anakin, 100.0, Color.yellow);
+        anakin.turnRight(90);
+        anakin.setLocation(112.87, 239.61);
+        nakresliKornout (anakin, 175.0, Color.orange);
+    }
+
+
+    private void nakresliKornout(Turtle anakin, double velikostZakladny, Color barva) {
+        anakin.setPenColor(barva);
+        anakin.move(velikostZakladny);
+        anakin.turnRight(113.7);
+        anakin.move(218.3);
+        anakin.turnRight(132.75);
+        anakin.move(218.3);
+        anakin.turnRight(23.625);
+}
+
+    private void nakresliCtverec(Turtle anakin, double velikostStrany, Color barva) {
+        double uhel = 90;
+        for (int i = 0; i < 4; i++) {
+            anakin.setPenColor(barva);
+            anakin.move(velikostStrany);
+            anakin.turnRight(uhel);
+
+        }
+    }
+
+    private void nakresliObdelnik(Turtle anakin, double velikostStranyA, double velikostStranyB, Color barva) {
+        double uhel = 90;
+        for (int i = 0; i < 2; i++) {
+            anakin.setPenColor(barva);
+            anakin.move(velikostStranyA);
+            anakin.turnRight(uhel);
+            anakin.move(velikostStranyB);
+            anakin.turnRight(uhel);
+        }
+    }
+
+    private void nakresliRovnostrannyTrojuhelnik(Turtle anakin, double velikostStrany, Color barva) {
         double uhel = 120;
         for (int i = 0; i < 3; i++) {
-            anakin.setPenColor(barva);
             anakin.move(velikostStrany);
             anakin.turnRight(uhel);
         }
     }
 
-    private void nakresliCtverec(Turtle padme, double velikostStrany, Color barva) {
-        double uhel = 90;
-        for (int i = 0; i < 4; i++) {
-            padme.setPenColor(barva);
-            padme.move(velikostStrany);
-            padme.turnRight(uhel);
-
-        }
-    }
-
-    private void nakresliObdelnik(Turtle leia, double velikostStranyA, double velikostStranyB, Color barva) {
-        double uhel = 90;
-        for (int i = 0; i < 2; i++) {
-            leia.setPenColor(barva);
-            leia.move(velikostStranyA);
-            leia.turnRight(uhel);
-            leia.move(velikostStranyB);
-            leia.turnRight(uhel);
-        }
-    }
-
-    private void nakresliKolo(Turtle luke, double polomer, Color barva) {
+    private void nakresliKolo(Turtle anakin, double polomer, Color barva) {
         double uhel = 12;
         double pi = 3.141592653589;
         double obvod = 30;
         double neco = (polomer*2*pi)/obvod;
         for (int i = 0; i < 30; i++) {
-            luke.setPenColor(barva);
-            luke.move(neco);
-            luke.turnRight(uhel);
+            anakin.setPenColor(barva);
+            anakin.move(neco);
+            anakin.turnRight(uhel);
         }
     }
+
+    private void nakresliPluh(Turtle anakin, double delkaStrany, Color barva) {
+        anakin.setPenColor(barva);
+        anakin.turnRight(45);
+        anakin.move(60);
+        anakin.turnRight(135);
+        anakin.move(delkaStrany);
+        anakin.turnRight(90);
+        anakin.move(delkaStrany);
+}
 }
 
