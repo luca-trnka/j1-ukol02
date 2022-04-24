27984e57d765a1c59f88b98392b2aae185d06ab6
luca-trnka <luie-trnkova76@seznam.cz>
2022-04-24 22:32:14 +0200

    2.část zmrzka, sněhulák a mašinka

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -10,10 +10,55 @@ public class HlavniProgram {
         Turtle anakin;
         anakin = new Turtle();
 
-
-      //  nakresliZmrzku(anakin);
+        nakresliZmrzku(anakin);
 
         nakresliSnehulaka(anakin);
+
+        nakresliMašinku(anakin);
+
+
+    }
+
+    private void nakresliMašinku(Turtle anakin) {
+        anakin.setLocation(900, 300);
+        nakresliPluh(anakin, 100.0, Color.green);
+
+        anakin.setLocation(900, 370);
+        anakin.turnLeft(45);
+        nakresliObdelnik(anakin, 100,200, Color.green);
+
+        anakin.turnRight(90);
+        anakin.move(320);
+        anakin.turnRight(180);
+        nakresliObdelnik(anakin, 120,180, Color.green);
+
+        anakin.move(120);
+        anakin.turnRight(90);
+        nakresliKolo(anakin, 60.0, Color.black);
+
+        anakin.penUp();
+        anakin.turnLeft(90);
+        anakin.move(90);
+        anakin.turnLeft(180);
+        anakin.penDown();
+        nakresliKolo(anakin, 25.0, Color.black);
+
+        anakin.turnLeft(180);
+        anakin.penUp();
+        anakin.move(70);
+        anakin.turnLeft(180);
+        anakin.penDown();
+        nakresliKolo(anakin, 25.0, Color.black);
+    }
+
+    private void nakresliPluh(Turtle anakin, double velikostStrany, Color barva){
+     anakin.setPenColor(barva);
+     anakin.turnLeft(180);
+     anakin.move(velikostStrany);
+     anakin.turnRight(90);
+     anakin.move(velikostStrany);
+     anakin.turnRight(135);
+     anakin.move(Math.sqrt((Math.pow(velikostStrany,2))+(Math.pow(velikostStrany,2))));
 
 
     }
@@ -94,14 +139,7 @@ public class HlavniProgram {
         }
     }
 
-    private void nakresliPluh(Turtle anakin, double delkaStrany, Color barva) {
-        anakin.setPenColor(barva);
-        anakin.turnRight(45);
-        anakin.move(60);
-        anakin.turnRight(135);
-        anakin.move(delkaStrany);
-        anakin.turnRight(90);
-        anakin.move(delkaStrany);
+
 }
-}
+
 
